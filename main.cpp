62db88129fce417f86ebae1e3df062fa1c545b5a
Michael Victorino <michaelvictorino@gmail.com>
2024-12-20 00:02:37 -0500

    idk whats wrong, llvm-objcopy no such file or direcotry

--- main.cpp
+++ main.cpp
@@ -1,7 +1,55 @@
 #include <SDL2/SDL.h>
 
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
+struct Context 
+{
+        SDL_Renderer* renderer;
+};
+
+void emscripten_loop(void* arg)
+{
+        Context* context = static_cast<Context*>(arg);
+        SDL_Event event;
+        while(SDL_PollEvent(&event))
+        {
+                if(event.type == SDL_QUIT)
+                {
+                        exit(0);
+                }
+        }
+        SDL_SetRenderDrawColor(context->renderer, 9, 20, 33, 255);
+        SDL_RenderClear(context->renderer);
+        SDL_RenderPresent(context->renderer);
+}
+
+void main_loop(void* arg)
+{
+        Context* context = static_cast<Context*>(arg);
+        SDL_Event event;
+        while(true)
+        {
+                while(SDL_PollEvent(&event))
+                {
+                        if(event.type == SDL_QUIT)
+                        {
+                                exit(0);
+                        }
+                }
+                SDL_SetRenderDrawColor(context->renderer, 9, 20, 33, 255);
+                SDL_RenderClear(context->renderer);
+                SDL_RenderPresent(context->renderer);
+        }
+
+}
+
 int main(int argc, char** argv) 
 {
+        SDL_Init(SDL_INIT_EVERYTHING);
+
+        Context context;
         SDL_Window* window = NULL;
         window = SDL_CreateWindow(
                 "SDL2 It's Work!",
@@ -10,20 +58,16 @@ int main(int argc, char** argv)
         SDL_WINDOW_SHOWN
         );
         SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
-        SDL_SetRenderDrawColor(renderer, 9, 20, 33, 255);
+        context.renderer = renderer;
+        
+        #ifdef __EMSCRIPTEN__
+                emscripten_set_main_loop_arg(emscripten_loop, &context, -1, 1);
+        #else
+                main_loop(&context);
+        #endif
 
-        while(true){
-                SDL_Event event;
-                while(SDL_PollEvent(&event)){
-                        if( event.type == SDL_QUIT ){
-                                exit(0);
-                        }
-                }
-                SDL_RenderClear(renderer);
-                SDL_RenderPresent(renderer);
-  }
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
   //SDL_Quit();
-  return 0;
+        return 0;
 }
